9e0ab60d38ab5149d7b8c1812b7cbbd6bff53835
isaac.mccracken@gmail.com <isaac.mccracken@gmail.com>
2023-11-30 20:56:01 -0700

    basetypes

---
+++ include/basetypes.h
@@ -0,0 +1,17 @@
+
+#ifndef BASETYPES_H
+#define BASETYPES_H
+
+// Unsigned Integers
+typedef unsigned char U8;
+typedef unsigned short U16;
+typedef unsigned long U32;
+typedef unsigned long long U64;
+
+// Signed Integers
+typedef signed short      I16;
+typedef signed char       I8;
+typedef signed long       I32;
+typedef signed long long  I64;
+
+#endif // BASETYPES_H
