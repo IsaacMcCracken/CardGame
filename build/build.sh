9716f061292d6b63d0782e4547c4d086aede962f
Kaiden Keane <kjkeane88@gmail.com>
2023-12-17 00:34:10 -0700

    added files

---
+++ build/build.sh
@@ -0,0 +1,14 @@
+name="card game"
+
+source_directory=../src/*.c
+include_directory="../include/"
+library_directory="../lib"
+frameworks=(CoreVideo IOKit Cocoa GLUT OpenGL)
+
+other=(../lib/libraylib.a)
+
+for framework in "${frameworks[@]}"; do
+    framework_flags+=("-framework" "$framework")
+done
+
+clang -o "$name" $source_directory -I $include_directory -L $library_directory "${framework_flags[@]}" "${other[@]}"
