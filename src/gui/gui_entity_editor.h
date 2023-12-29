b90fd9fc1ef7547b7f35894488cdfe03491270f7
Isaac McCraken <isaac.mccracken@gmail.com>
2023-12-28 19:22:10 -0700

    editor is better

--- src/gui/gui_entity_editor.h
+++ src/gui/gui_entity_editor.h
@@ -1,6 +1,6 @@
+
 /*******************************************************************************************
-*
-*   EntityEditor v0.01 - Tool Description
+*   EntityEditor v1.0.0 - Tool Description
 *
 *   MODULE USAGE:
 *       #define GUI_ENTITY_EDITOR_IMPLEMENTATION
@@ -19,50 +19,47 @@
 *
 **********************************************************************************************/
 
-#include "raylib.h"
-
+#include <raylib.h>
 #include <raygui.h>
+
 #include <string.h>     // Required for: strcpy()
 
 #ifndef GUI_ENTITY_EDITOR_H
 #define GUI_ENTITY_EDITOR_H
 
 typedef struct {
-    bool EntityEditorBoxActive;
-    bool EntityNameTextBoxEditMode;
-    char EntityNameTextBoxText[128];
+    // Define anchors
+    Vector2 anchor;            // ANCHOR ID:1
+    
+    // Define controls variables
+    bool EntityEditorWindowBoxActive;            // WindowBox: EntityEditorWindowBox
+    bool NameTextBoxEditMode;
+    char NameTextBoxText[128];            // TextBox: NameTextBox
     bool HealthSpinnerEditMode;
-    int HealthSpinnerValue;
+    int HealthSpinnerValue;            // Spinner: HealthSpinner
     bool MovementSpinnerEditMode;
-    int MovementSpinnerValue;
-
-    Rectangle layoutRecs[6];
+    int MovementSpinnerValue;            // Spinner: MovementSpinner
+    int EntityFactionToggleGroupActive;            // ToggleGroup: EntityFactionToggleGroup
+    bool PlayableCheckBoxExChecked;            // CheckBoxEx: PlayableCheckBoxEx
+    bool EnemyableCheckBoxExChecked;            // CheckBoxEx: EnemyableCheckBoxEx
+    bool FlamableCheckBoxExChecked;            // CheckBoxEx: FlamableCheckBoxEx
+    bool FreezableCheckBoxExChecked;            // CheckBoxEx: FreezableCheckBoxEx
+    bool WetableCheckBoxExChecked;            // CheckBoxEx: WetableCheckBoxEx
 
     // Custom state variables (depend on development software)
     // NOTE: This variables should be added manually if required
 
 } GuiEntityEditorState;
-
 #ifdef __cplusplus
 extern "C" {            // Prevents name mangling of functions
 #endif
 
 //----------------------------------------------------------------------------------
-// Defines and Macros
-//----------------------------------------------------------------------------------
-//...
-
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
-// ...
-
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
+// Module Functions Declaration\n\
 //----------------------------------------------------------------------------------
 GuiEntityEditorState InitGuiEntityEditor(void);
 void GuiEntityEditor(GuiEntityEditorState *state);
-static void EntityEditorSaveButton();
+static void EntitySaveButton();                // Button: EntitySaveButton logic
 
 #ifdef __cplusplus
 }
@@ -77,57 +74,55 @@ static void EntityEditorSaveButton();
 ************************************************************************************/
 #if defined(GUI_ENTITY_EDITOR_IMPLEMENTATION)
 
-#include "raygui.h"
 
-//----------------------------------------------------------------------------------
-// Global Variables Definition
-//----------------------------------------------------------------------------------
-//...
-
-//----------------------------------------------------------------------------------
-// Internal Module Functions Definition
-//----------------------------------------------------------------------------------
-//...
-
-//----------------------------------------------------------------------------------
-// Module Functions Definition
-//----------------------------------------------------------------------------------
 GuiEntityEditorState InitGuiEntityEditor(void)
 {
     GuiEntityEditorState state = { 0 };
 
-    state.EntityEditorBoxActive = true;
-    state.EntityNameTextBoxEditMode = false;
-    strcpy(state.EntityNameTextBoxText, "Entity Name");
+    // Init anchors
+    state.anchor = (Vector2){ 0, 0 };            // ANCHOR ID:1
+    
+    // Initilize controls variables
+    state.EntityEditorWindowBoxActive = true;            // WindowBox: EntityEditorWindowBox
+    state.NameTextBoxEditMode = false;
+    strcpy(state.NameTextBoxText, "Name");            // TextBox: NameTextBox
     state.HealthSpinnerEditMode = false;
-    state.HealthSpinnerValue = 0;
+    state.HealthSpinnerValue = 0;            // Spinner: HealthSpinner
     state.MovementSpinnerEditMode = false;
-    state.MovementSpinnerValue = 0;
-
-    state.layoutRecs[0] = (Rectangle){ 0, 0, 280, 584 };
-    state.layoutRecs[1] = (Rectangle){ 8, 32, 32, 24 };
-    state.layoutRecs[2] = (Rectangle){ 56, 32, 216, 24 };
-    state.layoutRecs[3] = (Rectangle){ 56, 64, 216, 24 };
-    state.layoutRecs[4] = (Rectangle){ 56, 96, 216, 24 };
-    state.layoutRecs[5] = (Rectangle){ 232, 552, 40, 24 };
+    state.MovementSpinnerValue = 0;            // Spinner: MovementSpinner
+    state.EntityFactionToggleGroupActive = 0;            // ToggleGroup: EntityFactionToggleGroup
+    state.PlayableCheckBoxExChecked = false;            // CheckBoxEx: PlayableCheckBoxEx
+    state.EnemyableCheckBoxExChecked = false;            // CheckBoxEx: EnemyableCheckBoxEx
+    state.FlamableCheckBoxExChecked = false;            // CheckBoxEx: FlamableCheckBoxEx
+    state.FreezableCheckBoxExChecked = false;            // CheckBoxEx: FreezableCheckBoxEx
+    state.WetableCheckBoxExChecked = false;            // CheckBoxEx: WetableCheckBoxEx
 
     // Custom variables initialization
 
     return state;
 }
 
-
 void GuiEntityEditor(GuiEntityEditorState *state)
 {
-    if (state->EntityEditorBoxActive)
+    // Draw controls
+    if (state->EntityEditorWindowBoxActive)
     {
-        state->EntityEditorBoxActive = !GuiWindowBox(state->layoutRecs[0], "Entity Editor");
-        if (GuiTextBox(state->layoutRecs[2], state->EntityNameTextBoxText, 128, state->EntityNameTextBoxEditMode)) state->EntityNameTextBoxEditMode = !state->EntityNameTextBoxEditMode;
-        if (GuiSpinner(state->layoutRecs[3], "Health    ", &state->HealthSpinnerValue, 0, 100, state->HealthSpinnerEditMode)) state->HealthSpinnerEditMode = !state->HealthSpinnerEditMode;
-        if (GuiSpinner(state->layoutRecs[4], "Movement", &state->MovementSpinnerValue, 0, 100, state->MovementSpinnerEditMode)) state->MovementSpinnerEditMode = !state->MovementSpinnerEditMode;
-        if (GuiButton(state->layoutRecs[5], "Save")) EntityEditorSaveButton(); 
+        state->EntityEditorWindowBoxActive = !GuiWindowBox((Rectangle){ state->anchor.x + 0, state->anchor.y + 0, 272, 480 }, "Entity Editor");
+        GuiLabel((Rectangle){ state->anchor.x + 8, state->anchor.y + 32, 32, 24 }, "Name");
+        if (GuiTextBox((Rectangle){ state->anchor.x + 40, state->anchor.y + 32, 224, 24 }, state->NameTextBoxText, 128, state->NameTextBoxEditMode)) state->NameTextBoxEditMode = !state->NameTextBoxEditMode;
+        if (GuiSpinner((Rectangle){ state->anchor.x + 104, state->anchor.y + 64, 160, 24 }, "Health Capacity    ", &state->HealthSpinnerValue, 0, 100, state->HealthSpinnerEditMode)) state->HealthSpinnerEditMode = !state->HealthSpinnerEditMode;
+        if (GuiSpinner((Rectangle){ state->anchor.x + 104, state->anchor.y + 96, 160, 24 }, "Movement Capacity", &state->MovementSpinnerValue, 0, 100, state->MovementSpinnerEditMode)) state->MovementSpinnerEditMode = !state->MovementSpinnerEditMode;
+        if (GuiButton((Rectangle){ state->anchor.x + 144, state->anchor.y + 448, 120, 24 }, "Save")) EntitySaveButton(); 
+        GuiToggleGroup((Rectangle){ state->anchor.x + 16, state->anchor.y + 144, 80, 24 }, "Neutral;Player;Enemy", &state->EntityFactionToggleGroupActive);
+        GuiLabel((Rectangle){ state->anchor.x + 96, state->anchor.y + 184, 64, 16 }, "Entity Flags");
+        GuiCheckBox((Rectangle){ state->anchor.x + 8, state->anchor.y + 200, 24, 24 }, "Playable", &state->PlayableCheckBoxExChecked);
+        GuiLabel((Rectangle){ state->anchor.x + 112, state->anchor.y + 128, 40, 16 }, "Faction");
+        GuiCheckBox((Rectangle){ state->anchor.x + 96, state->anchor.y + 200, 24, 24 }, "Enemyable", &state->EnemyableCheckBoxExChecked);
+        GuiCheckBox((Rectangle){ state->anchor.x + 184, state->anchor.y + 200, 24, 24 }, "Flamable", &state->FlamableCheckBoxExChecked);
+        GuiLine((Rectangle){ state->anchor.x + 8, state->anchor.y + 168, 256, 16 }, NULL);
+        GuiCheckBox((Rectangle){ state->anchor.x + 8, state->anchor.y + 232, 24, 24 }, "Freezeable", &state->FreezableCheckBoxExChecked);
+        GuiCheckBox((Rectangle){ state->anchor.x + 96, state->anchor.y + 232, 24, 24 }, "Wetable", &state->WetableCheckBoxExChecked);
     }
-    GuiLabel(state->layoutRecs[1], "Name");
 }
 
 #endif // GUI_ENTITY_EDITOR_IMPLEMENTATION
